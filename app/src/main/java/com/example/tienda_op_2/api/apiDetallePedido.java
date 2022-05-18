549184af6a37f49e73ada32dd545c596a816699b
Boris <85261574+Boxvi@users.noreply.github.com>
2022-05-18 11:06:23 -0500

    change

--- app/src/main/java/com/example/tienda_op_2/api/apiDetallePedido.java
+++ app/src/main/java/com/example/tienda_op_2/api/apiDetallePedido.java
@@ -4,7 +4,7 @@ import com.example.tienda_op_2.Services.DetalleService;
 
 public class apiDetallePedido {
 
-    public static final String BASE_URL = "http://192.168.18.245:8080/";
+    public static final String BASE_URL = "http://10.0.2.2:8080/";
 
     public static DetalleService getDetalle() {
         return  servicioApi.getRetro(BASE_URL).create(DetalleService.class);
