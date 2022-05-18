b98029dea4bded69a4b06e9d7e0cdceb3d9ce38f
Boris <85261574+Boxvi@users.noreply.github.com>
2022-05-18 11:10:00 -0500

    Update Apis.java

--- app/src/main/java/com/example/tienda_op_2/weblogin/utildades/Apis.java
+++ app/src/main/java/com/example/tienda_op_2/weblogin/utildades/Apis.java
@@ -2,7 +2,7 @@ package com.example.tienda_op_2.weblogin.utildades;
 
 public class Apis {
 
-    public static final String BASE_URL = "http://192.168.18.245:8080/usuario/";
+    public static final String BASE_URL = "http://10.0.2.2:8080/usuario/";
 
 
     public static UsuarioService getUsuarioService() {
