e845fb66b8004de0af8914a8d8b327c554d5da6d
Boris <85261574+Boxvi@users.noreply.github.com>
2022-05-18 11:09:41 -0500

    Update ApiClient.java

--- app/src/main/java/com/example/tienda_op_2/weblogin/utildades/ApiClient.java
+++ app/src/main/java/com/example/tienda_op_2/weblogin/utildades/ApiClient.java
@@ -1,7 +1,7 @@
 package com.example.tienda_op_2.weblogin.utildades;
 
 public class ApiClient {
-    public static final String URL = "http://192.168.18.245:8080/";
+    public static final String URL = "http://10.0.2.2:8080/";
 
     public static ClienteService getClienteService() {
         return Client.getClient(URL).create(ClienteService.class);
